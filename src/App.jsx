2fa8670175830db4047fdf0e09d1cb71f5454d8e
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-11 22:34:12 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,41 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import   './App.css' 
-
+import './App.css';
 
 const App = () => {
-    const [toDos, setToDos] = useState(() => {
-        const storedToDos = localStorage.getItem('toDos');
-        return storedToDos ? JSON.parse(storedToDos) : [];
-       });
- const [task, setTask] = useState('');
- const [description, setDescription] = useState('');
- const [filter, setFilter] = useState('all');
+  const [toDos, setToDos] = useState(() => {
+    const storedToDos = localStorage.getItem('toDos');
+    return storedToDos ? JSON.parse(storedToDos) : [];
+  });
+  const [task, setTask] = useState('');
+  const [description, setDescription] = useState('');
+  const [filter, setFilter] = useState('all');
 
-
- const saveToDos = () => {
+  const saveToDos = () => {
     localStorage.setItem('toDos', JSON.stringify(toDos));
-   };  
+  };
 
-
- const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (task !== '' && description !== '') {
       setToDos([...toDos, { task, description }]);
       setTask('');
       setDescription('');
     }
- };
+  };
 
- const handleFilterChange = (e) => {
+  const handleFilterChange = (e) => {
     setFilter(e.target.value);
- };
+  };
 
- const handleDelete = (index) => {
+  const handleDelete = (index) => {
     let newToDos = [...toDos];
     newToDos.splice(index, 1);
     setToDos(newToDos);
     saveToDos();
- };
+  };
 
- const handleEdit = (index) => {
+  const handleEdit = (index) => {
     const updatedTask = prompt('Enter updated task name');
     const updatedDescription = prompt('Enter updated description');
     if (updatedTask !== '' && updatedDescription !== '') {
@@ -47,22 +44,21 @@ const App = () => {
       setToDos(newToDos);
       saveToDos();
     }
- };
+  };
 
-
- const handleComplete = (todo) => {
+  const handleComplete = (todo) => {
     setToDos(
-      toDos.map((index) => {
-        if (index.task === todo.task) {
-          return { ...index, completed: !index.completed };
+      toDos.map((item) => {
+        if (item.task === todo.task) {
+          return { ...item, completed: !item.completed };
         }
-        return index;
+        return item;
       })
     );
     saveToDos();
- };
+  };
 
- const visibleToDos = toDos.filter((todo) => {
+  const visibleToDos = toDos.filter((todo) => {
     if (filter === 'all') {
       return true;
     } else if (filter === 'completed') {
@@ -70,56 +66,54 @@ const App = () => {
     } else if (filter === 'uncompleted') {
       return !todo.completed;
     }
- });
+  });
 
- return (
+  return (
     <div>
-      <Header/>
+      <Header />
       <form onSubmit={handleSubmit} id='formItem'>
-        <input id='inputBox1'
+        <input
+          id='inputBox1'
           type="text"
           placeholder="Task"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
         <br />
-        <input id='inputBox2'
-          type="text"
+        <textarea
+          id='inputBox2'
           placeholder="Description"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-        /><br />
+        />
+        <br />
         <button id='submitBtn' type="submit">Add Task</button>
       </form>
       <br />
       <div id='filterItems'>
-      <label >Status Filter</label>
-      <select value={filter} onChange={handleFilterChange} >
-        <option value="all">All</option>
-        <option value="completed">Completed</option>
-        <option value="uncompleted">Uncompleted</option>
-      </select>
+        <label>Status Filter</label>
+        <select value={filter} onChange={handleFilterChange}>
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
       </div>
-      
       <div className='listItems'>
         {visibleToDos.map((todo, index) => (
-          <div key={index}className='taskItems'>
+          <div key={index} className='taskItems'>
             <h3>{todo.task}</h3>
+            <hr />
             <p>{todo.description}</p>
-            <button className="btnItems"onClick={() => handleDelete(index)}>Delete</button>
-            <button className="btnItems"onClick={() => handleEdit(index)}>Edit</button>
-            <button className="btnItems"onClick={() => handleComplete(todo)}>
+            <button className="btnItems" onClick={() => handleDelete(index)}>Delete</button>
+            <button className="btnItems" onClick={() => handleEdit(index)}>Edit</button>
+            <button className="btnItems" onClick={() => handleComplete(todo)}>
               {todo.completed ? 'Completed' : 'Mark as Completed'}
             </button>
-            
           </div>
         ))}
       </div>
     </div>
- );
+  );
 };
 
 export default App;
-
-
-
